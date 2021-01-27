5cfc30b692c9f5aa5a7a7676174227d5eacdda3c
James Velasquez <xyberviri@gmail.com>
2021-01-27 16:36:17 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,7 +1,20 @@
+var calendartz = 'America/New_York'; 
+
+var hash = window.location.hash.substr(1);
+var result = hash.split('&').reduce(function (res, item) {
+    var parts = item.split('=');
+    res[parts[0]] = parts[1];
+    return res;
+}, {});
+if (typeof result.timezone !== 'undefined') {
+    calendartz = result.timezone;     
+}
+
+
 var googleDataUrl='https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&color=%239E69AF&showPrint=0&ctz=';
 function setCalendarUrl(t) {
   if (t === undefined) {
-    t = 'America%2FNew_York';
+    t = calendartz;
   }
   var x,calendarString = '';
 	
@@ -12,20 +25,21 @@ function setCalendarUrl(t) {
   $("#change").prop("data",newurl); 
 }
 
-function setTime(t){
-  if (t === undefined) {
-    t = 'UTC';
+function setTime(t){    
+  if (t === undefined) {    
+    t = calendartz;
   }
+  
 	var time = new Date();
-	document.getElementById('clock').innerHTML = "Current <b>"+t+"</b> time: " + time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true, timeZone: t });
+	document.getElementById('clock').innerHTML = "Current " + t + " time: " + time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true, timeZone: t });
 }
-	
+
 $(document).ready(function() {
     $("a").click(function(){
     var me = $(this),
-    data = me.data('key');
-    setCalendarUrl(data.timezone);
-    setTime(data.code);
+    data = me.data('key');    
+    setCalendarUrl(data.timezone);    
+    setTime(data.timezone);        
     });    
 setCalendarUrl();
 setTime();
